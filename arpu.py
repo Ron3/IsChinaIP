5cb9d292115c09f1131dfcccdc4254ab4d254158
Ron3 <luodm03@qq.com>
2017-03-24 12:22:17 +0800

    -change: 计算arpu值的脚本.

---
+++ arpu.py
@@ -0,0 +1,74 @@
+#coding=utf-8
+"""
+Create On 2017/3/24
+
+@author: Ron2
+"""
+
+
+import pymysql
+
+
+class DB(object):
+    """
+    """
+    def __init__(self):
+        self.conn = None
+        self.pidSet = set()
+
+
+    def start(self):
+        """
+        开始
+        :return:
+        """
+        self._initConn()
+        self._initPidSet()
+        self._getPayLog()
+
+
+    def _getPayLog(self):
+        """
+        得到付费信息
+        :return:
+        """
+        pidArray = tuple(self.pidSet)
+        strPidArray = str(pidArray)
+        for sid in xrange(7001, 7028, 1):
+            if sid == 7002:
+                continue
+
+            serverName = "ron_server_%s" % sid
+            cur = self.conn.cursor()
+            sql = "user %s" % serverName
+            cur.execute(sql)
+
+            sql = "select pid, amount from pay where pid in %s " % strPidArray
+            print cur.execute(sql)
+
+
+    def _initConn(self):
+        """
+        初始化连接
+        :return:
+        """
+        self.conn = pymysql.connect(host='10.8.38.34', port=3306, user='root', passwd='adgjmpdb0', db='mysql')
+
+
+    def _initPidSet(self):
+        """
+        初始化pid
+        :return:
+        """
+        fileObj = open("./hkPidSet", "rb")
+        data = fileObj.read()
+        fileObj.close()
+
+        self.pidSet = eval(data)
+        print "len(self.pidSet)==> ", len(self.pidSet)
+
+
+
+if __name__=="__main__":
+    db = DB()
+    db.start()
